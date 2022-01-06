4404381de69df4dc22e8e7d9f9db179e476960a8
christian8bits <christian8bits@gmail.com>
2022-01-06 18:42:13 -0300

    Lista 1 exercicios 5 a 10

---
+++ pyzumbis/lista1/lista1_e05.py
@@ -0,0 +1,8 @@
+# Solicite o preço de uma mercadoria e o percentual de desconto. 
+# Exiba o valor do desconto e o preço a pagar.
+m = float(input('Preço da mercadoria: '))
+p = float(input('Porcentagem de desconto: '))
+desconto = m * p / 100
+novo = m - desconto
+print('Desconto: R$ %.2f' %desconto)
+print('Preço a pagar: R$ %.2f' %novo)
