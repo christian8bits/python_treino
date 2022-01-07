ada0b7554b37aa1cf28e23492ad6d36521792f25
christian8bits <christian8bits@gmail.com>
2022-01-07 17:37:59 -0300

    atualizado

--- pyzumbis/lista1/lista1_e04.py
+++ pyzumbis/lista1/lista1_e04.py
@@ -1,6 +1,8 @@
-#Faça um programa que calcule o aumento de um salário. 
-#Ele deve solicitar o valor do salário e a
-#porcentagem do aumento. Exiba o valor do aumento e do novo salário.
+"""
+Faça um programa que calcule o aumento de um salário. 
+Ele deve solicitar o valor do salário e a
+porcentagem do aumento. Exiba o valor do aumento e do novo salário.
+"""
 s = float(input('Salario: '))
 p = float(input('Porcentagem de aumento: '))
 aumento = s * p / 100
