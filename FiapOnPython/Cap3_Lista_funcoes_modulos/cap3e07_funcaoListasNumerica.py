d8cb542523697bfe9494d3ea2463b1da869dfff1
ChristianBits <christian8bits@gmail.com>
2022-01-09 17:37:07 -0300

    Cap3 finalizado, funcao e pacotes

---
+++ FiapOnPython/Cap3_Lista_funcoes_modulos/cap3e07_funcaoListasNumerica.py
@@ -0,0 +1,5 @@
+valores=[10.11, 33.54, 74.12, 143.13, 86.38, 93.07, 6.99, 107.28, 114.74, 53.89]
+print(valores)
+print("O mais caro custa: ", max(valores))
+print("O mais barato custa: ", min(valores))
+print("O total dos valores é de: ", sum(valores))
