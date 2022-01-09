ff4309ef7a89a7729f212bc2e87fc30452538e46
ChristianBits <christian8bits@gmail.com>
2022-01-08 21:35:13 -0300

    Adicionado lista 3 Extra

---
+++ pyzumbis/lista3/lista3.5_e01.py
@@ -0,0 +1,10 @@
+"""
+Dizemos que um número natural é triangular se ele é produto de três números naturais consecutivos. Exemplo: 120 é triangular, pois 4.5.6 = 120. Dado um inteiro não-negativo n, verificar se n é triangular.
+"""
+n = int(input('N: '))
+k = 0
+while k * (k + 1) * (k + 2) < n:
+  k = k + 1
+
+print(k* (k+1)* k+2 == n)
+
