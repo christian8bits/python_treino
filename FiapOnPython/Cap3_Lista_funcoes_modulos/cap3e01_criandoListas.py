5170dc8c1b55cb7fa1c50d54b1a2b5a97808dac8
ChristianBits <christian8bits@gmail.com>
2022-01-09 09:49:16 -0300

    cap 3 Criando listas

---
+++ FiapOnPython/Cap3_Lista_funcoes_modulos/cap3e01_criandoListas.py
@@ -0,0 +1,14 @@
+#lista preenchida estaticamente
+lista_estatica = ['xpto', True]
+print('Lista estatica')
+print(lista_estatica)
+
+#lista preenchida dinamicamente
+print('\nLista dinamica')
+lista_dinamica = [input('Digite o usuário: '), bool(int(input('Está logado? \n(0 para NAO / 1 para SIM): ')))]
+print(lista_dinamica)
+
+#lista vazia
+lista_vazia=[]
+print('\nLista vazia')
+print(lista_vazia)
