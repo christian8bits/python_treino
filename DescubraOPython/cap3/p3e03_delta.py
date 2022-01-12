b53d34e818355aedbbb9501dd741beb122ae82d9
christian8bits <christian8bits@gmail.com>
2022-01-12 18:13:04 -0300

    Descobrindo python Capitulo 3

--- DescubraOPython/cap3/p3e03_delta.py
+++ DescubraOPython/cap3/p3e03_delta.py
@@ -5,11 +5,11 @@ from datetime import datetime
 from datetime import timedelta
 
 def deltaTempo():
-    delta = timedelta(days = 86, hours = 8532, minutes = 7645)
-    print(delta)
+  delta = timedelta(days = 86, hours = 8532, minutes = 7645)
+  print(delta)
 
-    hoje = datetime.now()
-    print('Data no futuro: ' , hoje + delta)
-    print('Data no passado: ', hoje - delta)
+  hoje = datetime.now()
+  print('Data no futuro: ' , hoje + delta)
+  print('Data no passado: ', hoje - delta)
 
 deltaTempo()
