08db6b8d13f4a7bfc05357a89f4296c459c6518b
christian8bits <christian8bits@gmail.com>
2022-01-13 18:58:55 -0300

    exercicio 7 curso em video

---
+++ cursoEmVideoPython/e007_media.py
@@ -0,0 +1,5 @@
+# leia duas notas e calcule a media
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+media = (n1 + n2) / 2
+print(f'A media entre {n1} e {n2} é {media}')
