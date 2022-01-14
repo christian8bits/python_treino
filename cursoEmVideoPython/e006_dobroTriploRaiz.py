f22029975c65f29a40426e6f91504e9795fa0718
christian8bits <christian8bits@gmail.com>
2022-01-14 14:07:45 -0300

    CeV Python e006

---
+++ cursoEmVideoPython/e006_dobroTriploRaiz.py
@@ -0,0 +1,8 @@
+# leia um numero, mostre o dobro, o tripo e a raiz quadrade desse numero
+n = int(input('Digite um numero: '))
+d = n * 2
+t = n * 3
+r = n ** (1/2)
+print(f'O dobro de {n} é igual a {d}')
+print(f'O tripo de {n} é igual a  {t}')
+print(f'A raiz quadrada de {n} é igual a {r}')
