bcb8f9695017664f182085f6915714e8eb7e5ab1
christian8bits <christian8bits@gmail.com>
2022-01-27 10:46:56 -0300

    tipos embutidos

--- orientacaoObjetoPythonCaelum/cap03.01_tiposEmbutidos.py
+++ orientacaoObjetoPythonCaelum/cap03.01_tiposEmbutidos.py
@@ -1,2 +1,8 @@
-texto = "Aplicacao"
+texto = "isso ê texto"
+inteiro = 20
+decimal 45.78
+complexo 2 + 3j
 print(texto, type(texto))
+print(inteiro, type(inteiro))
+print(decimal, type(decimal))
+print(complexo, type(complexo))
