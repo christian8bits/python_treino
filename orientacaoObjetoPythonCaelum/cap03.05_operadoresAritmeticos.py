583871efc3b26241ce1d7a21bb4ac66aaf715f1d
christian8bits <christian8bits@gmail.com>
2022-01-28 10:01:05 -0300

    operadores aritmeticos

---
+++ orientacaoObjetoPythonCaelum/cap03.05_operadoresAritmeticos.py
@@ -0,0 +1,10 @@
+a = 8
+b = 3
+print(f'variavel a {a} variavel b {b}')
+print(f'soma {a+b}')
+print(f'subtracao {a-b}')
+print(f'multiplicacao {a*b}')
+print(f'divisao {a/b}')
+print(f'potenciacao {a**b}')
+print(f'divisao inteira {a//b}')
+print(f'resto de divisao {a%b}')
