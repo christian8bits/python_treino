07cad465f9f6e0125c2134ce6ae17ef794cf528b
christian8bits <christian8bits@gmail.com>
2022-01-28 10:29:08 -0300

    strings

---
+++ orientacaoObjetoPythonCaelum/cap03.06_strings.py
@@ -0,0 +1,7 @@
+texto1 = 'hello'
+texto2 = 'python'
+print(f'String 1: {texto1} String 2: {texto2}')
+print(f'Soma strings {texto1 + texto2}')
+print(f'multiplica string {texto2 * 3}')
+print(f'Caixa Alta {texto2.upper()}')
+print(f'Capitalizado {texto2.capitalize()}')
