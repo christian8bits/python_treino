6a2d694a57a09eb365db745646f270550605e173
christian8bits <christian8bits@gmail.com>
2022-01-30 23:44:33 -0300

    km por litro

---
+++ URIOnlineJudge/iniciante/1014_consumo.py
@@ -0,0 +1,7 @@
+#Leia quatro valores correspondentes a (x,y). calcule a distância entre eles, mostrando 4 casas decimais após a vírgula.
+
+X=int(input())
+Y=float(input())
+
+c=X/Y
+print("%.3f km/l" %c)
