36a71c9645e5294128f3cb0171d465c47a6e5045
christian8bits <christian8bits@gmail.com>
2022-01-31 12:07:04 -0300

    Conversor de temperatura Celsius para Farenheit

---
+++ cursoEmVideoPython/e014_temperatura.py
@@ -0,0 +1,4 @@
+# temperatura de Celsius para Farenheit
+celsius = float(input('Digite a temperatura em Celsius: '))
+farenheit = ((1.8 * celsius) + 32)
+print('{}ºC correspondem a {:.1f}ºF'.format(celsius, farenheit))
