d255f774f43c2a398f171b0404c463f2c8189383
christian8bits <christian8bits@gmail.com>
2022-02-02 14:56:46 -0300

    Quebrando numero inteiro

---
+++ cursoEmVideoPython/e016_quebrandoNumero.py
@@ -0,0 +1,4 @@
+# Leia um número real qualquer pelo teclado e mostre na tela a sua porção inteira.
+
+num = float(input("Digite um número real: \n"))
+print("O valor digitado é {} e a sua porção inteira é {}.".format(num, int(num)))
