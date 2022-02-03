7747c455305a27cb29fe896a5d276b451348b051
christian8bits <christian8bits@gmail.com>
2022-02-03 09:08:17 -0300

    distancia

---
+++ URIOnlineJudge/iniciante/1016_distancia.py
@@ -0,0 +1,13 @@
+'''
+ Dois carros (x a 60km e y a 90km).
+ Leia a distância (km) e calcule quanto
+ tempo leva para y tomar distência de x.
+'''
+
+X=60
+Y=90
+
+num=int(input())
+
+dist=num*2
+print("%d minutos" %dist)
