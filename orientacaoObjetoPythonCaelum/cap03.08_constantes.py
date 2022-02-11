2e6f15fd0b21419faae14f4ffa9cc634d8f8143f
christian8bits <christian8bits@gmail.com>
2022-02-11 11:11:19 -0300

    55

--- orientacaoObjetoPythonCaelum/cap03.08_constantes.py
+++ orientacaoObjetoPythonCaelum/cap03.08_constantes.py
@@ -1,5 +1,20 @@
 '''
 valores Boleanos como True, False sao constantes, assim tambem como None, que representa abstencao de valor
 '''
-print(f'1 == 4 é {1 == 4}')
-
+print(f'1 == 4 é igual? {1 == 4}')
+print(f'3 < 8  é menor? {3 < 8}')
+print(f'7 > 9 é maior? {7 > 9}')
+print(f'4 != 5 é diferente? {4 != 5}')
+print(f'5 <= 5 é menor ou igual? {5 <= 5}')
+print(f'6 >= 9 é maior ou igual? {6 >= 9}')
+t = "Texto"
+print(f'"Texto" == "Texto" é igual? {t  == "Texto"}')
+print(f'bool("Texto") retorna {bool("Texto")}')
+print(f'bool("") retorna {bool("")}')
+print(f'bool(1) retorna {bool(1)}')
+print(f'bool(0) retorna {bool(0)}')
+print(f'bool(-20) retorna {bool(-20)}')
+print(f'bool(1.50) retorna {bool(1.50)})')
+print(f'bool(None) retorna {bool(None)}')
+print(f'3 is 3 é identico? {3 is 3}')
+print(f'4 is not 6 é identico? {}')
