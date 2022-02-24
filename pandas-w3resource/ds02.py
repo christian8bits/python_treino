7401bbfd99fd3087308909a749062c6c2790bfc0
christian8bits <christian8bits@gmail.com>
2022-02-24 14:02:09 -0300

    Data Series Pandas

---
+++ pandas-w3resource/ds02.py
@@ -0,0 +1,8 @@
+import pandas as pd
+ds = pd.Series([2, 4, 6, 8, 10])
+print('Pandas Series e Type')
+print(ds)
+print(type(ds))
+print('Converter Pandas Series para Lista python')
+print(ds.tolist())
+print(type(ds.tolist()))
