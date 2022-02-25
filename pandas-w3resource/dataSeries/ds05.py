bd43227b4fe46ccc4cd5ac922c34c2454cc77f3e
christian8bits <christian8bits@gmail.com>
2022-02-25 15:21:17 -0300

    Panda Data Series

---
+++ pandas-w3resource/dataSeries/ds05.py
@@ -0,0 +1,8 @@
+import pandas as pd
+d1 = {'a': 100, 'b': 200, 'c':300, 'd':400, 'e':800}
+print("Dicionario:")
+print(d1)
+serieConvertida = pd.Series(d1)
+print("Convertido para series:")
+print(serieConvertida)
+
