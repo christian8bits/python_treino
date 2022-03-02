030e3d5796d779e90598cb3c73f8cecc24734b23
christian8bits <christian8bits@gmail.com>
2022-03-02 18:23:23 -0300

    Pandas Data Series

---
+++ pandas-w3resource/dataSeries/ds14.py
@@ -0,0 +1,7 @@
+import pandas as pd
+s = pd.Series(data = [1,2,3,4,5], index = ['A', 'B', 'C','D','E'])
+print("Data Series Original:")
+print(s)
+s = s.reindex(index = ['B','A','C','D','E'])
+print("Data Series após alterar a ordem do índice:")
+print(s)
