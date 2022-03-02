aa074084f8fe19ad36a7d2b57137dac0b89751cc
christian8bits <christian8bits@gmail.com>
2022-03-02 18:27:04 -0300

    Pandas Data Series

---
+++ pandas-w3resource/dataSeries/ds15.py
@@ -0,0 +1,8 @@
+import pandas as pd
+s = pd.Series(data = [1,2,3,4,5,6,7,8,9,5,3])
+print("Data Series Original:")
+print(s)
+print("Média da Série de Dados:")
+print(s.mean())
+print("Desvio padrão da Série de Dados:")
+print(s.std())
