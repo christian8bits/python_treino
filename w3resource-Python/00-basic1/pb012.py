fb1c60de98181416473f295161a2c8330efd9583
christian8bits <christian8bits@gmail.com>
2022-03-10 17:13:47 -0300

    Python Basic 1

---
+++ w3resource-Python/00-basic1/pb012.py
@@ -0,0 +1,5 @@
+import calendar
+a = int(input('Entre com o ano: '))
+m = int(input('Entre com o mês: '))
+print('\n')
+print(calendar.month(a,m))
---
+++ w3resource-Python/00-basic1/pb013.py
@@ -0,0 +1,12 @@
+print('''
+Eu amo tudo o que foi
+Tudo o que já não é
+A dor que já me não dói
+A antiga e errônea fé
+O ontem que a dor deixou,
+O que deixou alegria
+Só porque foi, e voou
+E hoje é já outro dia.
+
+--Fernando Pessoa 
+''')
