b601adda488c8a95185756ad43d4ff2aa9fa7b41
christian8bits <christian8bits@gmail.com>
2022-03-16 16:36:32 -0300

    Python Basic 1

--- w3resource-Python/00-basic1/pb017.py
+++ w3resource-Python/00-basic1/pb017.py
@@ -1,6 +1,6 @@
-def near_thousand(n):
-      return ((abs(1000 - n) <= 100) or (abs(2000 - n) <= 100))
-print(near_thousand(1000))
-print(near_thousand(900))
-print(near_thousand(800))   
-print(near_thousand(2200))
+def proximo_mil(n):
+  return ((abs(1000 - n) <= 100) or (abs(2000 - n) <= 100))
+print(proximo_mil(1000))
+print(proximo_mil(900))
+print(proximo_mil(800))   
+print(proximo_mil(2200))
--- w3resource-Python/00-basic1/pb018.py
+++ w3resource-Python/00-basic1/pb018.py
@@ -1,6 +1,8 @@
-def near_thousand(n):
-      return ((abs(1000 - n) <= 100) or (abs(2000 - n) <= 100))
-print(near_thousand(1000))
-print(near_thousand(900))
-print(near_thousand(800))   
-print(near_thousand(2200))
+def soma_tres(x, y, z):
+  soma = x + y + z
+  if x == y == z:
+    soma = soma * 3
+  return soma
+
+print(soma_tres(1, 2, 3))
+print(soma_tres(3, 3, 3))
