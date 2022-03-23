9053cb66adc2e2a10a205b719fd503b835373a9c
christian8bits <christian8bits@gmail.com>
2022-03-23 16:37:39 -0300

    Python Basic 1

---
+++ w3resource-Python/00-basic1/pb022.py
@@ -0,0 +1,11 @@
+def contar4_lista(lista):
+  contador = 0
+  for n in lista:
+    if n == 4:
+      contador = contador + 1
+  return contador
+
+lista1 = [1, 3, 4, 5, 6, 4]
+lista2 = [4, 6, 9, 7, 4, 3, 6, 4, 8 ]
+print(contar4_lista(lista1))
+print(contar4_lista(lista2))
