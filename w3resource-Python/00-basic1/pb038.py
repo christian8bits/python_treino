a18dfb35c8516108a2fc07d3d375b73d6876f2a1
christian8bits <christian8bits@gmail.com>
2022-03-28 17:14:00 -0300

    Python Basic 1

---
+++ w3resource-Python/00-basic1/pb038.py
@@ -0,0 +1,3 @@
+x, y = 4, 3
+resultado = x * x + 2 * x * y + y * y
+print('({} + {}) ^ 2) = {}'.format(x, y, resultado))
---
+++ w3resource-Python/00-basic1/pb039.py
@@ -0,0 +1,5 @@
+amt = 1000
+int = 3.5
+anos = 7
+valor_futuro = amt*((1+(0.01*int)) ** anos)
+print(round(valor_futuro, 2))
