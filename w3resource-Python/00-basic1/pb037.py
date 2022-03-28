d359fc31a9263386c41d2a2031d6fbb33f441454
christian8bits <christian8bits@gmail.com>
2022-03-28 17:04:16 -0300

    Python Basic 1

---
+++ w3resource-Python/00-basic1/pb037.py
@@ -0,0 +1,6 @@
+def detalhes_pessoais():
+  nome, idade = "Chris", 29
+  endereco = "Brasil, SP, São Paulo"
+  print("Nome: {}\nIdade: {}\nEndereço: {}".format(nome, idade, endereco))
+
+detalhes_pessoais()
