66498f074281f00211f08c027ce68b3005e0f933
christian8bits <christian8bits@gmail.com>
2022-04-11 18:42:59 -0300

    Python Basic 1

---
+++ w3resource-Python/00-basic1/pb058.py
@@ -0,0 +1,6 @@
+'''
+58. Escreva um programa Python para somar os primeiros n inteiros positivos.
+'''
+n = int(input("Entre com um numero: "))
+sum_num = (n * (n + 1)) / 2
+print("a soma dos primeiros", n ,"inteiros positivos:", sum_num)
