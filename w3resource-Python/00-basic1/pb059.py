c6356f8bb321f998f968a8366601755456957faf
christian8bits <christian8bits@gmail.com>
2022-04-12 18:42:13 -0300

    Python Basic 1

---
+++ w3resource-Python/00-basic1/pb059.py
@@ -0,0 +1,12 @@
+'''
+59. Escreva um programa Python para converter altura (em pés e polegadas) em centímetros.
+'''
+print("Entre com sua altura: ")
+h_ft = int(input("Digite o tamanho em pés: "))
+h_inch = int(input("Digite o tamanho em polegadas: "))
+
+h_inch += h_ft * 12
+h_cm = round(h_inch * 2.54, 1)
+
+print("Sua altura é : %d cm." % h_cm)
+
