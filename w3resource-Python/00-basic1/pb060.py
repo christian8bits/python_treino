ad8176ca0947fd0fa6a48a3d66741a52afd25e89
ChristianBits <christian8bits@gmail.com>
2022-04-15 22:43:01 -0300

    Python Basic 1

---
+++ w3resource-Python/00-basic1/pb060.py
@@ -0,0 +1,10 @@
+'''
+60. Escreva um programa em Python para calcular a hipotenusa de um triângulo retângulo.
+'''
+from math import sqrt
+print("Entre com os lados de um triângulo retângulo:")
+a = float(input("lado a: "))
+b = float(input("lado b: "))
+c = sqrt(a**2 + b**2)
+print("A hipotenusa é:", c )
+
